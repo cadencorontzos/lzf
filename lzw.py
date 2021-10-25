4986768333cc4debb6d8bda3e4be48a6b6147f0c
cadencorontzos <cadencorontzos@gmail.com>
2021-10-24 18:52:33 -0700

    major changes - first iteration of compress(), pass around filename instead of whole file

--- lzw.py
+++ lzw.py
@@ -6,21 +6,38 @@
 import sys
 import time
 import random
+import os
 
-def compress(text):
+def compress(filename, newfilename):
+    codebook = {}
+    file = open(filename, 'r')
+    newFile = open(newfilename, 'w')
+    codeword = 0
+    nextChar = file.read(1)
+    currentBlock = ''
+    #does this run too many times?
+    while nextChar:
+        currentBlock = str(currentBlock) + nextChar
+        if codebook.get(currentBlock):
+            currentBlock = codebook.get(currentBlock)
+        else:
+            codebook[currentBlock] = codeword
+            codeword+=1
+            newFile.write(currentBlock)
+            currentBlock = ''
+        nextChar = file.read(1)
+    file.close()
+    newFile.close()
+
+def decompress(filename, newFileName):
     return('test')
 
-def decompress(text):
-    return('test')
+def printHeader(what):
+    print('---------------------------------------')
+    print(what.upper()+':')
 
-def printSample(text, which):
-    print('_____________________________________________')
-    print('Here is a sample of the ' + which + ' file.')
-    print(text[1000:1500])
-    print('_____________________________________________')
-
-def reportSize(text, which):
-    size = len(text.encode('utf-8'))
+def reportSize(filename, which):
+    size = os.path.getsize(filename)
     print('The size of the ' + which + ' file is '+ str(size) + ' bytes.')
     return size
 
@@ -28,42 +45,56 @@ def reportTime(start, end, what):
     totalTime = end - start
     print('The time of ' + what + ' was ' + str(totalTime) + ' seconds.')
 
-def reportCompressionRate(original, compressed):
+def reportCompressionRate(originalFileName, compressedFileName):
+    original = os.path.getsize(originalFileName)
+    compressed = os.path.getsize(compressedFileName)
     rate = original/compressed
     print('The compression rate was ' + str(rate) + '.')
 
-def generateOutputFile(which, file, filename):
-    f= open( which +'-'+filename+'-'+str(randSuffix) + '.txt', 'w')
-    f.write(file)
-    f.close()
+def makeFilename(which, filename):
+    return which +'-'+filename+'-'+str(randSuffix) + '.txt'
+
+def printFooter(names):
+    print('---------------------------------------')
+    print('To see the three versions of the file, see ', end = '')
+    print(*names, sep=', ', end='.\n')
 
 # generates a report of the compression/decompression process of a given file
 if __name__ == "__main__":
     randSuffix = random.randint(10000,20000)
+    names = []
 
     #original
+    printHeader('original')
     filename = sys.argv[-1]
     fileData = open(filename)
     file = fileData.read()
-    original = reportSize(file, 'original')
-    printSample(file,'original')
-    generateOutputFile('original',file, filename)
+    original = reportSize(filename, 'original')
+    originalFileName = makeFilename('original', filename)
+    f= open(originalFileName, 'w')
+    f.write(file)
+    f.close()
+    names.append(originalFileName)
 
     #compression
+    printHeader('compression')
+    compressedFileName = makeFilename('compressed', filename)
+    names.append(compressedFileName) 
     start = time.time()
-    compressedFile = compress(file)
-    generateOutputFile('compressed', compressedFile, filename)
+    compressedFile = compress(filename, compressedFileName)
     end = time.time()
     reportTime(start, end, 'compression')
-    compressed = reportSize(compressedFile, 'compressed')
-    reportCompressionRate(original, compressed)
-    printSample(compressedFile, 'compressed')
-
+    reportSize(compressedFileName, 'compressed')
+    reportCompressionRate(filename, compressedFileName)
 
     #decompression
+    printHeader('decompression')
+    decompressedFileName = makeFilename('decompressed', filename)
     start = time.time()
-    decompressedFile = decompress(compressedFile)
-    generateOutputFile('decompressed', decompressedFile, filename)
+    decompressedFile = decompress(compressedFileName, decompressedFileName)
     end = time.time()
+    names.append(decompressedFileName)
     reportTime(start, end, 'decompresssion')
-    printSample(decompressedFile, 'decompressed')
+    # reportSize(decompressedFileName, 'decompressed')
+
+    printFooter(names)
